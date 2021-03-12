b8c631749f4f99320bceac40b261384b4e6b0df7
Kamila Pereira <kamila@welow.com.br>
2021-03-12 13:52:10 -0300

    First commit

---
+++ projeto-vue/src/plugins/vuetify.js
@@ -0,0 +1,24 @@
+import Vue from "vue";
+import Vuetify from "vuetify/lib/";
+
+Vue.use(Vuetify);
+
+import colors from "vuetify/lib/util/colors";
+
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.purple,
+        secondary: colors.grey.darken1,
+        accent: colors.shades.black,
+        error: colors.red.accent3,
+        background: colors.indigo.lighten5,
+      },
+      dark: {
+        primary: colors.blue.lighten3,
+        background: colors.indigo.base,
+      },
+    },
+  },
+});
